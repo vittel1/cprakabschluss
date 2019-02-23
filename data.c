669cd28d34282ff4b897e73b300dd1be11a18769
marcel <marcelpape@msn.com>
2019-02-23 22:12:33 +0100

    Beschreibung: char Array, new_string und hash

--- data.c
+++ data.c
@@ -5,17 +5,18 @@
 #include <stdio.h>
 
 /*
-TODO: Char Array
+Strings sind eine Sequenz von chars, die in einem Array gespeichert werden. Das Nullbyte '\0' zeigt das Ende des Strings an.
+Die Variable 'strings' wird im Struct auf die Adresse vom ersten Element des Arrays zeigen.
 
-Um später einfacher Strings vergleichen zu können, wird die Länge im Attribut 'laenge' gespeichert.
+Um später einfacher Strings vergleichen zu können, wird die Länge in der Variable 'laenge' gespeichert.
 
-Reference counting: Der Struct bekommt ein Attribut, das zählt wie oft das aktuelle Objekt referenziert wird.
+Reference counting: Der Struct bekommt eine Variable, die zählt wie oft das aktuelle Objekt referenziert wird.
 Jedes mal, wenn Speicher allokiert wird oder eine Referenz auf das Objekt erzeugt wird, erhöht sich der Zähler.
 Zu beachten ist, dass der Counter bei 1 startet.
 Wenn der Zeiger auf das Objekt entfernt wird, dann wird der Counter dekrementiert. Ist der Counter 0 gibt es keine
 Referenzen mehr auf das Objekt und es kann deallokiert werden (wie es auch ein Garbage Collector tun würde).
 
-Um zu unterscheiden, ob es sich um einen String oder Blob handelt, wird ein Attribut 'typ' erzeugt.
+Um zu unterscheiden, ob es sich um einen String oder Blob handelt, wird eine Variable 'typ' erzeugt.
 Dabei steht '0' für einen String und '1' für einen Blob.
 */
 
@@ -28,43 +29,52 @@ struct data
 };
 
 /*
+Im ersten Teil wird Speicher für den Struct allokiert. Als Größe wird die Größe des Structs angegeben.
+Wenn kein Speicher allokiert werden kann, dann wird das durch das Programm abgefangen. Durch die fehlende
+Fehlerbehandlung in C muss die extra angegeben werden. Nur wenn erfolgreich allokiert wurde, können Werte zugewiesen werden.
 
- "content" is a null-terminated string.
- Verwendung von 'strdup', um den Speicher dynamisch zu allokieren. Es wird ein Pointer auf den allokierten Speicher zurückgegeben.
+Die Funktion 'strlen' gibt die Länge des Strings zurück. Das Nullbyte am Ende wird nicht gezählt.
 
+Es wird 'strdup' verwendet, um den Speicher dynamisch zu allokieren. Es wird ein Pointer auf den allokierten Speicher zurückgegeben.
  */
 data* data_new_string (char const* content)
 {
   struct data *data;
   data = malloc(sizeof(struct data));
-  //TODO ist das notwendig? Stand das nichtmal in den Folien?
   if(data == NULL) {
+    //TODO gibts besseres als NULL?
     return NULL;
   }
-  data->refcount = 1;
-  data->typ = 0;
-  data->laenge = strlen(content);
-  data->string = strdup(content);
-  return data;
+  else {
+    data->refcount = 1;
+    data->typ = 0;
+    data->laenge = strlen(content);
+    data->string = strdup(content);
+    return data;
+  }
 }
 
-/* "content" is a blob of length "length". */
+/* "
+content" is a blob of length "length".
+*/
 data* data_new_blob (char const* content, unsigned int length)
 {
   struct data *data;
   data = malloc(sizeof(struct data));
-  //TODO ist das notwendig? Stand das nichtmal in den Folien?
   if(data == NULL) {
+    //TODO gibts besseres als NULL?
     return NULL;
   }
-  data->refcount = 1;
-  data->typ = 1;
-  data->laenge = length;
-  data->string = malloc(sizeof(char) * (length + 1));
-  for(unsigned int i = 0; i < length; i++) {
-    data->string[i] = content[i];
+  else {
+    data->refcount = 1;
+    data->typ = 1;
+    data->laenge = length;
+    data->string = malloc(sizeof(char) * (length + 1));
+    for(unsigned int i = 0; i < length; i++) {
+      data->string[i] = content[i];
+    }
+    return data;
   }
-  return data;
 }
 
 /*
@@ -97,7 +107,7 @@ TODO Beschreibung asprintf
 char* data_as_string (data const* data)
 {
   char* result;
-  if(data->typ == 0){
+  if(data->typ == 0) {
     asprintf(&result, "String: %s", data->string);
   }
   else {
@@ -108,9 +118,9 @@ char* data_as_string (data const* data)
 
 /*
 
-TODO Beschreibung Hashfunktion
-
-Ist angelegt an den djb2 Hashalgorithmus.
+Der im folgenden implementierte Algorithmus (djb2) ist angelegt an die folgenden:
+https://gist.github.com/conmarap/e1a802d0f462531bc712b5505b59d5e8
+http://www.cse.yorku.ca/~oz/hash.html
 
 */
 unsigned int data_hash (data const* data)
